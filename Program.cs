ac9077ba696c8e7a10e165c74e048b16002eb31f
Ivan Djikovski <ivan.djikovski@wsa.com>
2023-11-29 20:20:01 +0100

    Trying with real db

--- Program.cs
+++ Program.cs
@@ -7,26 +7,22 @@ var builder = WebApplication.CreateBuilder(args);
 
 var connString = builder.Configuration.GetConnectionString("TestConnectionString");
 
-//builder.Services.AddDbContext<TestApiContext>(options => 
-//    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDbContext<TestApiContext>(options =>
+    options.UseSqlServer(connString));
 
 var app = builder.Build();
 
+app.MapGet("/", async (TestApiContext dbContext) =>
+{
+    dbContext.User.Add(new User("Ivan", "Djikovski"));
+    await dbContext.SaveChangesAsync();
 
+    return "Hello World!";
+});
 
-app.MapGet("/", () => connString ?? "It is null");
-
-//app.MapGet("/", async (TestApiContext dbContext) =>
-//{
-//    dbContext.User.Add(new User("Ivan", "Djikovski"));
-//    await dbContext.SaveChangesAsync();
-
-//    return "Hello World!";
-//});
-
-//app.MapGet("/users", (TestApiContext dbContext) =>
-//{
-//    return dbContext.User.Select(x => x);
-//});
+app.MapGet("/users", (TestApiContext dbContext) =>
+{
+    return dbContext.User.Select(x => x);
+});
 
 app.Run();
